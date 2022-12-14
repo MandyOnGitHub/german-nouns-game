165fb45f5e634215f85548c4a9caba0171cd9032
MandyOnGitHub <Mandy.mailen@yahoo.com>
2022-12-14 20:26:00 +0100

    add randomize nouns and toggle flashcard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.scss'
 import axios from 'axios'
+import * as tools from './tools'
 
 
 
@@ -10,6 +11,8 @@ interface INoun {
   article: string;
   singular: string;
   plural: string;
+  isOpen: Boolean,
+  isLearned: Boolean
 }
 
 function App() {
@@ -17,22 +20,44 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const response = await axios.get(nounsUrl)
-      setNouns(response.data)
+      let response = (await axios.get(nounsUrl)).data
+      response = tools.randomize(response)
+      const _nouns: INoun[] = []
+      response.forEach((rawNoun: any) => {
+        const _noun = {
+          ...rawNoun,
+          isOpen: false,
+          isLearned: false,
+        }
+        _nouns.push(_noun)
+      })
+      setNouns(_nouns)
     })();
-  })
+  }, [])
 
-  
-  
+  const handleToggleFlashCard = (noun: INoun) => {
+      noun.isOpen = !noun.isOpen
+      setNouns([...nouns])
+  }
+
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>German Nouns</h1>
       <h2>There are {nouns.length} nouns.</h2>
-      <div className="nouns">
+      <div className='nouns'>
         {nouns.map((noun, key) => {
           return (
-            <div className="noun" key={noun.singular}>
-              {noun.singular}
+            <div className='noun' key={noun.singular}>
+              <div className='front' onClick={() => handleToggleFlashCard(noun)}>{noun.singular}</div>
+              {noun.isOpen && (
+                <div className='back'>
+                  <div className="singular">
+                    {noun.article} {noun.singular}
+                  </div>
+                  <div className="plural">{noun.plural}</div>
+                </div>
+              )}
             </div>
           )
         })}
